debabfec4cf4225ddf3d414d1a4ea53cdba21c85
King-khid <kingkhid60@gmail.com>
2025-09-04 09:35:24 +0000

    save sensitive informations to .env file

--- customerservice/customerservice/settings.py
+++ customerservice/customerservice/settings.py
@@ -11,15 +11,13 @@ SECRET_KEY = env('SECRET_KEY', default='django-insecure-468_xm&lb8^oz&52j4)c8zup
 
 DEBUG = env.bool('DEBUG', default=True)
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # or your SMTP host
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kingkhid60@gmail.com'
-EMAIL_HOST_PASSWORD = 'ynmq luoe ijxm cxog'
-DEFAULT_FROM_EMAIL = 'kingkhid60@gmail.com'
-#from_email = "CustomerService <kingkhid60@gmail.com>"
-
+EMAIL_BACKEND = env("EMAIL_BACKEND")
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env.int("EMAIL_PORT")
+EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
 
 ALLOWED_HOSTS = []
 
